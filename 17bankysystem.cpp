c9fba96fc5179b0e1be478b0c3d98b16f5928f8a
Chessar90 <petereliaschessar@yahoo.com>
2025-04-03 09:25:55 +0300

    Add files via upload

---
+++ 17bankysystem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Account {
+public:
+    std::string accountNumber;
+    std::string holderName;
+    double balance;
+
+    Account(std::string accNum, std::string name, double bal)
+        : accountNumber(accNum), holderName(name), balance(bal) {}
+
+    void display() {
+        std::cout << "Account Number: " << accountNumber 
+                  << "\nHolder: " << holderName 
+                  << "\nBalance: $" << balance << std::endl;
+    }
+};
+
